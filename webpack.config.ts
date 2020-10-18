f317bcd3d2112ae2667eaa9454202f9389e114e0
jjaehrig <jens-uwe.jaehrig@fsd-web.de>
2020-10-18 09:07:42 +0200

    update to latest packages

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,6 @@
-var path = require('path');
-var webpack = require('webpack');
-var webpackMerge = require('webpack-merge');
+const path = require('path');
+// import webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
 
 var baseConfig = {  
   target: 'async-node',
@@ -12,20 +12,24 @@ var baseConfig = {
     path: path.resolve(__dirname, './dist')
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
+    /*
+    new webpack.config.optimization.splitChunks({
       name: 'inline',
       filename: 'inline.js',
       minChunks: Infinity
     }),
+    */
+    /*
     new webpack.optimize.AggressiveSplittingPlugin({
         minSize: 5000,
         maxSize: 10000
     }),
+    */
   ]
 };
 
 let targets = ['web', 'webworker', 'node', 'async-node', 'node-webkit', 'electron-main'].map((target) => {
-  let base = webpackMerge(baseConfig, {
+  let base = webpackMerge.merge(baseConfig, {
     target: target,
     output: {
       path: path.resolve(__dirname, './dist/' + target),
